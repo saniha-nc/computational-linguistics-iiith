4f22e46da441bf1c5e1e41c32fd22709b784f4f2
Saniha N C <sanihanc@gmail.com>
2020-07-09 20:14:45 +0530

    functionalites related to button goal-5

--- RDIP/Codes/script.js
+++ RDIP/Codes/script.js
@@ -22,34 +22,88 @@ var sentence2 = [["राम और श्याम बाजार गयें
 ["एक लाल किताब वहाँ है", "एक लाल किताब है वहाँ", "वहाँ है एक लाल किताब", "है वहाँ एक लाल किताब"],
 ["एक बड़ी सी किताब वहाँ है", "एक बड़ी सी किताब है वहाँ", "बड़ी सी एक किताब वहाँ है", " बड़ी सी एक किताब है वहाँ", "वहाँ है एक बड़ी सी किताब", "वहाँ है बड़ी सी एक किताब", " है वहाँ एक बड़ी सी किताब", "है वहाँ बड़ी सी एक किताब"]];
 
+function intro(){
+	document.getElementById("s3").innerHTML = "Introduction";
+	document.getElementById("para1").innerHTML = "A sentence can become more complex, if more than one verb is present or by joining two sentences or words using conjunctions or by some other methods.";
+	document.getElementById("para2").innerHTML = "<br>In this experiment also, you will make more difficult sentences using the given words.";
+	document.getElementById("para11").innerHTML = "";
+	document.getElementById("para22").innerHTML = "";
+	document.getElementById("buttons").innerHTML = "";
+	document.getElementById("dropdown").innerHTML = "";
+	document.getElementById("san1").innerHTML = "";
+	document.getElementById("san2").innerHTML = "";
+	document.getElementById("reform").innerHTML = "";
+}
 function viewtheory(){
 	document.getElementById("s3").innerHTML = "Theory";
 	document.getElementById("para1").innerHTML = "<u>Clause</u><br>A clause typically contains a subject noun phrase and a finite verb. Some languages allow subjects to be omitted. There are two types of subclauses:<ol><li>Independent clause</li><li>Subordinate clause</li></ol>Independent clause shows the complete meaning in it. For example: Ram eats. A subordinate clause is not a complete sentence. For example: because I am sick. Sentences can also be classified on the basis of clauses.";
 	document.getElementById("para2").innerHTML = "<br>Classification on the basis of clauses are:<ol><li>A <b>Simple sentence</b> consists of only one independent clause. There are no subordinate clauses.</li><li>A <b>Compound sentence</b> consists of more than one independent clauses joined ny conjunctions or punctuations. There are no subordinate clauses.</li><li>A <b>Complex sentence</b> consists of atleast one indpendent clause and a single subordinate clause</li><li>A <b>Complex-compound </b>sentence consists of more than one independent clauses and atleast one of the independent clauses has one or more than one subordinate clauses</li></ol>";
+	document.getElementById("para11").innerHTML = "";
+	document.getElementById("para22").innerHTML = "";
+	document.getElementById("buttons").innerHTML = "";
+	document.getElementById("dropdown").innerHTML = "";
+	document.getElementById("san1").innerHTML = "";
+	document.getElementById("san2").innerHTML = "";
+	document.getElementById("reform").innerHTML = "";
 }
 function viewobjec(){
 	document.getElementById("s3").innerHTML = "Objective";
 	document.getElementById("para1").innerHTML = "<br><hr><br>The objective of this experiment is to know how to form logically correct sentences from the given words.";
 	document.getElementById("para2").innerHTML = "<br><hr><br>";
+	document.getElementById("para11").innerHTML = "";
+	document.getElementById("para22").innerHTML = "";
+	document.getElementById("buttons").innerHTML = "";
+	document.getElementById("dropdown").innerHTML = "";
+	document.getElementById("san1").innerHTML = "";
+	document.getElementById("san2").innerHTML = "";
+	document.getElementById("reform").innerHTML = "";
 }
 function quizzes(){
 	document.getElementById("s3").innerHTML = "Quizzes";
 	document.getElementById("para1").innerHTML = "Which of these is a valid sentence?<ol><li>Ram came after lunch.</li><li>Ram came after having lunch.</li><li>Sleeping I saw a tiger.</li><li>I saw a sleeping tiger.</li><li>No Parking is allowed.</li></ol><br>";
 	document.getElementById("para2").innerHTML = "<ol><li>खाते खाते राम सो गया</li><li>राम खाते खाते सो गया</li><li>राम खाता खाते सो गया</li><li>राम खाकर सो गया</li><li>राम और श्याम ने भी खाना खाया</li><li>राम ने और श्याम भी खाना खाया</li></ol>";
+	document.getElementById("para11").innerHTML = "";
+	document.getElementById("para22").innerHTML = "";
+	document.getElementById("buttons").innerHTML = "";
+	document.getElementById("dropdown").innerHTML = "";
+	document.getElementById("san1").innerHTML = "";
+	document.getElementById("san2").innerHTML = "";
+	document.getElementById("reform").innerHTML = "";
+}
+var wordstring = "";
+function answer(id,value){
+	document.getElementById("san2").style.textAlign = "center";
+	document.getElementById("san1").style.textAlign = "center"; 
+	document.getElementById("san1").style.color = "darkblue";
+	document.getElementById("san1").innerHTML = "<font color='darkblue'><b>Formed Sentence</b></font><font color='blue'> <i>(after selecting words)</i>:</font>";
+	wordstring += value ;
+	wordstring += " ";
+	document.getElementById("san2").innerHTML = wordstring;
+	document.getElementById(id).style.display = "none";
+	document.getElementById("reform").innerHTML = "<center><button id='re'>Re-form the Sentence</button></center>";
+
 }
 function dropexp(){
 	var a = document.getElementById('drop').value;
 	if(a == 'select'){
 		document.getElementById("para1").innerHTML = "";
 		document.getElementById("para2").innerHTML = "";
+		document.getElementById("para11").innerHTML = "";
+		document.getElementById("para22").innerHTML = "";
+		document.getElementById("buttons").innerHTML = "";
+		document.getElementById("dropdown").innerHTML = "";
+		document.getElementById("san1").innerHTML = "";
+		document.getElementById("san2").innerHTML = "";
+		document.getElementById("reform").innerHTML = "";
 	}
 	if(a == 'english'){
-		document.getElementById("para1").style.color = "darkblue";
-		document.getElementById("para1").style.textAlign = "center";
-		document.getElementById("para2").style.color = "blue";
-		document.getElementById("para2").style.textAlign = "center";
-		document.getElementById("para1").innerHTML = "<br><br><b>Form a sentence (Declarative or Interrogative or any other type) from the given words</b>";
-		document.getElementById("para2").innerHTML = "<i>(select the buttons in proper order)</i>";
+		wordstring = "";
+		document.getElementById("para11").style.color = "darkblue";
+		document.getElementById("para11").style.textAlign = "center";
+		document.getElementById("para22").style.color = "blue";
+		document.getElementById("para22").style.textAlign = "center";
+		document.getElementById("para11").innerHTML = "<br><br><b>Form a sentence (Declarative or Interrogative or any other type) from the given words</b>";
+		document.getElementById("para22").innerHTML = "<i>(select the buttons in proper order)</i>";
 		var ran1 = Math.floor(Math.random() * 9);
 		var sent = sentence1[ran1][0];
 		var shuf = sent.split(" ");
@@ -67,20 +121,23 @@ function dropexp(){
 		var content;
 		while(i<=shuf.length-1){
 			content = shuf[i];
-			btns = "<button id='button"+i+"' value='"+content+"'>"+content+"</button> &nbsp;&nbsp;";
+			btns = "<button id='button"+i+"'onclick='answer(this.id,this.value)' value='"+content+"'>"+content+"</button> &nbsp;&nbsp;";
 			finalbtns += btns;
 			i++;
 		}
 		document.getElementById("buttons").style.textAlign = "center";
 		document.getElementById("buttons").innerHTML = finalbtns;
+		
+		
 	}
 	if(a == 'hindi'){
-		document.getElementById("para1").style.color = "darkblue";
-		document.getElementById("para1").style.textAlign = "center";
-		document.getElementById("para2").style.color = "blue";
-		document.getElementById("para2").style.textAlign = "center";
-		document.getElementById("para1").innerHTML = "<br><br><b>Form a sentence (Declarative or Interrogative or any other type) from the given words</b>";
-		document.getElementById("para2").innerHTML = "<i>(select the buttons in proper order)</i>";
+		wordstring = "";
+		document.getElementById("para11").style.color = "darkblue";
+		document.getElementById("para11").style.textAlign = "center";
+		document.getElementById("para22").style.color = "blue";
+		document.getElementById("para22").style.textAlign = "center";
+		document.getElementById("para11").innerHTML = "<br><br><b>Form a sentence (Declarative or Interrogative or any other type) from the given words</b>";
+		document.getElementById("para22").innerHTML = "<i>(select the buttons in proper order)</i>";
 		var ran2 = Math.floor(Math.random() * 6);
 		var sent = sentence2[ran2][0];
 		var shuf = sent.split(" ");
@@ -98,7 +155,7 @@ function dropexp(){
 		var content;
 		while(i<=shuf.length-1){
 			content = shuf[i];
-			btns = "<button id='button"+i+"' value='"+content+"'>"+content+"</button> &nbsp;&nbsp;";
+			btns = "<button id='button"+i+"' onclick='answer(this.id,this.value)' value='"+content+"'>"+content+"</button> &nbsp;&nbsp;";
 			finalbtns += btns;
 			i++;
 		}
@@ -111,9 +168,21 @@ function experiment(){
 	document.getElementById("dropdown").innerHTML = "<select id='drop' onchange='dropexp();'><option value='select'>---Select Language--- </option><option value='english'>English</option><option value='hindi'>Hindi</option></select>"; 
 	document.getElementById("para1").innerHTML = "";
 	document.getElementById("para2").innerHTML = "";
+	document.getElementById("para11").innerHTML = "";
+	document.getElementById("para22").innerHTML = "";
+	document.getElementById("san1").innerHTML = "";
+	document.getElementById("san2").innerHTML = "";
+	document.getElementById("reform").innerHTML = "";
 }
 function procedure(){
 	document.getElementById("s3").innerHTML = "Procedure";
 	document.getElementById("para1").innerHTML = "<u><b>STEP 1:</b></u> Select a language which you know better<br><u><b>STEP 2:</b></u> Select the buttons which has words written on it, in a proper order<br><u><b>OUTPUT:</b></u> Group of words in a selected order will be shown";
 	document.getElementById("para2").innerHTML = "<u><b>NOTE:</b></u><br><ol><li>If a wrong sentence is formed,<button> Re-form the sentence </button> is available for re-setting.</li><li>You can check whether the formed sentence is a valid or not by clicking <button> Check the correctness of this sentence  </button></li><li>For a wrong sentence, you can get the correct sentence by clicking <button> Get correct sentence</button></li></ol>";
+	document.getElementById("para11").innerHTML = "";
+	document.getElementById("para22").innerHTML = "";
+	document.getElementById("buttons").innerHTML = "";
+	document.getElementById("dropdown").innerHTML = "";
+	document.getElementById("san1").innerHTML = "";
+	document.getElementById("san2").innerHTML = "";
+	document.getElementById("reform").innerHTML = "";
 }
